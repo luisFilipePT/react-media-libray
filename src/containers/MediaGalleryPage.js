833cd511650e994ea397daabd8722d78564ff695
Luís <lf@void.pt>
2016-12-02 17:06:09 +0000

    Create container for Media Gallery page and update routes

--- src/containers/MediaGalleryPage.js
+++ src/containers/MediaGalleryPage.js
@@ -1,3 +1,19 @@
 /**
  * Created by luis on 02/12/2016.
  */
+import React, { Component } from 'react';
+import { flickrImages, shutterStockVideos } from '../Api/api';
+
+class MediaGalleryPage extends Component {
+
+    componentDidMount() {
+        flickrImages('rain').then(images => console.log(images, 'Images'));
+        shutterStockVideos('rain').then(videos => console.log(videos, 'Videos'));
+    }
+
+    render() {
+        return (<div></div>)
+    }
+}
+
+export default MediaGalleryPage;
