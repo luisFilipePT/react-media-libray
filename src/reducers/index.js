bc7b15b652b251986936edca20dcdd56c7d43d4b
Luís <lf@void.pt>
2016-12-09 10:43:00 +0000

    setup reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by luis on 02/12/2016.
  */
+import { combineReducers } from 'redux';
+import images from 'imageReducer';
+import videos from 'videoReducer';
+
+const rootReducer = combineReducers({
+    images,
+    videos
+});
+
+export default rootReducer;
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,3 +1,7 @@
 /**
  * Created by luis on 02/12/2016.
  */
+export default {
+    images: [],
+    videos: []
+};
