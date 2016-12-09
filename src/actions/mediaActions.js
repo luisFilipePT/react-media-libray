5f55bcc2aa7bdf22a2deadd70b325cef89ca899b
Luís <lf@void.pt>
2016-12-09 10:30:53 +0000

    actions setup

--- src/actions/mediaActions.js
+++ src/actions/mediaActions.js
@@ -1,3 +1,19 @@
 /**
  * Created by luis on 02/12/2016.
  */
+ import * as types from '../constants/actionTypes';
+
+ export const selectedImageAction = (image) => ({
+     type: types.SELECTED_IMAGE,
+     image
+ });
+
+export const selectedVideoAction = (video) => ({
+    type: types.SELECTED_VIDEO,
+    video
+});
+
+export const searchMediaAction = (payload) => ({
+    type: types.SEARCH_MEDIA_REQUEST,
+    payload
+});
