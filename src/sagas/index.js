d2df004b8cc904efb59de01ee22782b025157d70
Luís <lf@void.pt>
2016-12-09 11:42:00 +0000

    sagas in progress

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,11 @@
 /**
  * Created by luis on 02/12/2016.
  */
+import { fork } from 'redux-saga/effects';
+import watchSearchMedia from './watcher';
+
+// Here, we register our watcher saga(s) and export as a single generator
+// function (startForeman) as our root Saga.
+export default function* startForman() {
+    yield fork(watchSearchMedia);
+}
